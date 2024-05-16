271975f51f8d7f785d7f3ce94cdb742a05cceb82
Pentalimbed <pentalimbed@gmail.com>
2024-05-16 14:23:30 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -117,8 +117,8 @@ def evaluate_error(args, model, test_loader):
 def create_model(args):
     model = I2S(num_classes=args.num_classes,
                 encoder=args.encoder,
-                projection_mode='harmonicS2',
-                feature_sphere_mode='spatialS2',
+                projection_mode='spatialS2',
+                feature_sphere_mode='harmonicS2',
                 sphere_fdim=args.sphere_fdim,
                 lmax=args.lmax,
                 train_grid_rec_level=args.train_grid_rec_level,
